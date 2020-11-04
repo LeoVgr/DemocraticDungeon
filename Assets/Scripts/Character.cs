e5ee1d9d335ec79632494f708ad1ca819a0adbb0
TOTH Benoit <47602460+Ahzenrei@users.noreply.github.com>
2020-11-04 17:22:55 +0100

    Basic character architecture

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    [SerializeField]
+    private float _initialLife;
+    public bool Busy { get; protected set; }
+
+    public float Life { get; protected set; }
+
+    public abstract void PlayAction(int index);
+
+    public void ReceiveDamage(float amount)
+    {
+        Life -= amount;
+        if (Life < 0)
+        {
+            Die();
+        }
+    }
+
+    public void ReceiveHeal(float amount)
+    {
+        if (Life + amount > _initialLife)
+        {
+            Life = _initialLife;
+            return;
+        }
+
+        Life += amount;
+    }
+
+    private void Die()
+    {
+        Debug.Log(gameObject.name + " dead");
+        return;
+    }
+
+}
