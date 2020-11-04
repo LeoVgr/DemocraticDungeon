a6564e5118fc56cd926fcda69a9793e417bbe671
Julien Vanderhaegen <dimitritomskiev@gmail.com>
2020-11-05 00:03:01 +0100

    First Commit

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,342 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class MainMenu : MonoBehaviourPunCallbacks
+{
+    //public GameObject roomJoinUI;
+
+    
+    public bool DEBUG_MODE = false;
+
+    public int NB_PLAYERS_MAX = 2;
+    public float TURN_TIME = 20.0f;
+
+    private string gameVersion = "0.1";
+    private string roomName = "Room 1";
+
+    public int nb_member = 1;
+
+    List<RoomInfo> createdRooms = new List<RoomInfo>();
+
+    private Text connectionStatus;
+    public bool isConnecting = false;
+    private bool gameLaunch = false;
+
+    public bool mode_choice = false;
+    private float timer_turn = 0.0f;
+    private bool refresh = false;
+
+    public int nb_votes;
+    public int nb_votes_action_1;
+    public int nb_votes_action_2;
+
+    PhotonView pv;
+
+    void Awake()
+    {
+        //a verifier
+        PhotonNetwork.AutomaticallySyncScene = true;
+        pv = GetComponent<PhotonView>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIManager.getInstance().activateMenu("start");
+        /*if (!PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.PhotonServerSettings.AppSettings.AppVersion = gameVersion;
+            PhotonNetwork.ConnectUsingSettings();
+        }*/
+        PlayerPrefs.DeleteAll();
+        Debug.Log("Connecting to Photon Network");
+
+        ConnectToPhoton();
+    }
+
+    public bool setVotes(int type,int nb)
+    {
+        bool res = false;
+        if (mode_choice)
+        {
+            nb_votes += nb;
+            if(nb_votes >= 0)
+            {
+                if (type == 0)
+                    nb_votes_action_1 += nb;
+                else
+                    nb_votes_action_2 += nb;
+
+                res = true;
+            }
+            else
+            {
+                nb_votes = 0;
+                res = false;
+            }
+        }
+        return res;
+    }
+
+    //etude d'un controleur avec les phase Began et End => pour mouvement
+    //Mais la problème avec vote, pour le moment boutons
+
+    [PunRPC]
+    public void afficherMsg(string msg)
+    {
+        Debug.Log("Message : "+msg);
+    }
+
+    public int getMembers()
+    {
+        return nb_member;
+    }
+
+    public bool setMembers(int n)
+    {
+        Debug.Log("Ici set members " + n);
+        nb_member += n;
+        if (nb_member < 1)
+        {
+            nb_member = 1;
+            return false;
+        }
+        return true;
+    }
+
+    public bool verifySelectColor(string color)
+    {
+        bool res = JoinRoom(color);
+        //set du nom du joueur
+
+        //verif dans jeu si déjà présente ?
+        //pour le moment juste selectionne
+        return res;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //wait players
+        if (!gameLaunch)
+        {
+            LaunchTheGame();
+        }
+        //core game here
+        else
+        {
+            //side core game
+#if UNITY_ANDROID
+              
+                /*if(refresh){
+                    UIManager.getInstance().activateMenu((mode_choice ? "choice" : "turn"));
+                    refresh = false;
+                    //pv.RPC("setRefresh",RpcTarget.All,false);
+                }*/
+#elif UNITY_STANDALONE_WIN
+            Debug.Log("Tourne sur la machine centrale !");
+                if ((Time.realtimeSinceStartup - timer_turn) > TURN_TIME)
+                {
+                    timer_turn = Time.realtimeSinceStartup;
+                    mode_choice = !mode_choice;
+                    //pv.RPC("setRefreshMode", RpcTarget.All, !mode_choice);
+                    pv.RPC("setRefreshMode", RpcTarget.Others, mode_choice);
+                    //RpcTarget.Others
+                refresh = false;
+                    Debug.Log("Changement de mode");
+                }
+#endif
+            /*if (Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.WindowsPlayer)
+            {
+
+            }
+
+            //side players + boss
+            if (Application.platform == RuntimePlatform.Android)
+            {
+
+            }*/
+        }
+    }
+
+    public void disconnected()
+    {
+        if (isConnecting)
+        {
+            if (PhotonNetwork.CurrentRoom != null && PhotonNetwork.IsMasterClient)
+            {
+                PhotonNetwork.CurrentRoom.IsOpen = false;
+            }
+            PhotonNetwork.Disconnect();
+        }
+    }
+
+    public override void OnConnected()
+    {
+        base.OnConnected();
+        isConnecting = true;
+        //connectionStatus.text = "Connected to Photon";
+        //connectionStatus.color = Color.green;
+        //set button active to join
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        isConnecting = false;
+        Debug.Log("Disconnected.");
+    }
+
+    /*
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("OnConnected");
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        Debug.Log("We have callback, update the room list");
+        createdRooms = roomList;
+    }*/
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Success to join de room");
+#if UNITY_STANDALONE_WIN
+            bool connected = PhotonNetwork.SetMasterClient(PhotonNetwork.LocalPlayer);
+            Debug.Log("Connected master est de " + connected);
+#endif
+        /*if (PhotonNetwork.IsMasterClient)
+        {
+            //set ui
+            //pc
+        }
+        else
+        {
+            //playerStatus
+            //smartphone
+        }*/
+    }
+
+   
+
+    public bool JoinRoom(string name)
+    {
+        bool connected = PhotonNetwork.IsConnected;
+        if (connected)
+        {
+            PhotonNetwork.LocalPlayer.NickName = name;
+            Debug.Log("Photon.Network.connected() | Trying to Create/Join Room");
+            RoomOptions roomOptions = new RoomOptions();
+            TypedLobby typedLobby = new TypedLobby("", LobbyType.Default);
+            //pas vraiment if faudrait verifier si elle existe avant pour que le pc soit le master
+            
+            PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, typedLobby);
+
+            //#if UNITY_ANDROID
+                    
+                //PhotonNetwork.CurrentRoom.IsOpen = false;
+                //Debug.Log("Creation Room "+connected+"    "+ PhotonNetwork.CountOfRooms+" et "+PhotonNetwork.CurrentRoom.Name);
+            //pour pc
+            /*else if (Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.WindowsPlayer)
+            {
+                connected = PhotonNetwork.CreateRoom(roomName, roomOptions, typedLobby);
+                PhotonNetwork.CurrentRoom.IsOpen = false;
+                Debug.Log("Creation Room");
+            }
+            //pour smartphone
+            else if (Application.platform == RuntimePlatform.Android)
+                connected = PhotonNetwork.JoinRoom(roomName);
+            */
+        }
+        return connected;
+    }
+
+    void ConnectToPhoton()
+    {
+        //connectionStatus.text = "Connecting...";
+        PhotonNetwork.GameVersion = gameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    [PunRPC]
+    void setRefreshMode(bool state)
+    {
+        mode_choice = state;
+        UIManager.getInstance().activateMenu((mode_choice ? "choice" : "turn"));
+        if (mode_choice)
+        {
+            nb_votes = nb_member;
+            nb_votes_action_1 = nb_votes_action_2 = 0;
+        }
+
+        //refresh = true;
+    }
+
+    [PunRPC]
+    void setLaunch(bool state)
+    {
+        gameLaunch = state;
+    }
+
+    public void LaunchTheGame()
+    {
+        if (isConnecting)
+        {
+            if(PhotonNetwork.CurrentRoom != null)
+            {
+                if(PhotonNetwork.CurrentRoom.PlayerCount >= NB_PLAYERS_MAX)
+                {
+                    //pv = PhotonView.Get(this);
+                    timer_turn = Time.realtimeSinceStartup;
+#if UNITY_ANDROID
+                        UIManager.getInstance().activateMenu("turn");
+#elif UNITY_STANDALONE_WIN
+                    pv.RPC("setLaunch", RpcTarget.All, true);
+                    UIManager.getInstance().activateMenu("");
+#endif
+                }
+                else
+                {
+                    UIManager.getInstance().refreshNbJoueur(PhotonNetwork.CurrentRoom.PlayerCount);
+                    #if UNITY_ANDROID
+                                pv.RPC("afficherMsg",RpcTarget.Others,"ceci est un message !");
+                    #endif
+                }
+            }
+        }
+    }
+
+    /*
+    public void JoinRoom()
+    {
+        if (PhotonNetwork.connected)
+        {
+            PhotonNetwork.player.NickName = "";
+            Debug.Log("Photon.Network.connected() | Trying to Create/Join Room");
+            RoomOptions roomOptions = new RoomOptions();
+            TypedLobby typedLobby = new TypedLobby("", LobbyType.Default);
+            PhotonNetwork.JoinOrCreateRoom("", roomOptions, typedLobby);
+        }
+    }
+    public void LoadArena()
+    {
+        if(PhotonNetwork.room.PlayerCount > 1)
+        {
+            PhotonNetwork.LoadLevel("MainArena");
+        }
+        else
+        {
+            //Minimum 2 Players required to Load Arena!;
+        }
+    }
+    */
+    /*public override void OnConnected()
+    {
+        ;
+    }*/
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,210 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private MainMenu menu_manager;
+
+    static UIManager instance = null;
+
+    [SerializeField]
+    private GameObject StartMenu;
+    [SerializeField]
+    private GameObject SelectMenu;
+    [SerializeField]
+    private GameObject WaitingMenu;
+    [SerializeField]
+    private GameObject GameChoice;
+    [SerializeField]
+    private GameObject GameTurn;
+    [SerializeField]
+    private GameObject Number;
+
+    [SerializeField]
+    private string str_wait = "Waiting Room\nPlayers : ";
+
+    [SerializeField]
+    private string str_members = "Members : ";
+
+    [SerializeField]
+    private string str_votes_restants = "Votes Restants :\n";
+
+
+    public int nb_joueur = 0;
+
+    //Button
+    [SerializeField]
+    Button bt_start_start;
+    [SerializeField]
+    Button bt_start_quit;
+
+    [SerializeField]
+    Button bt_select_green;
+    [SerializeField]
+    Button bt_select_red;
+    [SerializeField]
+    Button bt_select_blue;
+    [SerializeField]
+    Button bt_select_white;
+    [SerializeField]
+    Button bt_select_black;
+    [SerializeField]
+    Button bt_select_pink;
+    [SerializeField]
+    Button bt_select_quit;
+
+    [SerializeField]
+    Button bt_members_plus;
+    [SerializeField]
+    Button bt_members_minus;
+
+    [SerializeField]
+    Button bt_attaque_1;
+    [SerializeField]
+    Button bt_attaque_2;
+    [SerializeField]
+    Button bt_attaque_1_m;
+    [SerializeField]
+    Button bt_attaque_2_m;
+    [SerializeField]
+    Button bt_validate;
+
+
+    public Text wait_txt;
+    public Text members_txt;
+    public Text votes_restants_txt;
+
+
+    // Start is called before the first frame update
+
+    public static UIManager getInstance()
+    {
+        return instance;
+    }
+
+    public void refreshNbJoueur(int nb)
+    {
+        nb_joueur = nb;
+        wait_txt.text = str_wait + nb_joueur;
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+
+        //set up des Boutons
+        bt_start_start.onClick.AddListener(clickStart);
+        bt_start_quit.onClick.AddListener(clickQuit);
+
+        bt_select_green.onClick.AddListener(() => { clickColor("green");});
+        bt_select_red.onClick.AddListener(() => { clickColor("red"); });
+        bt_select_blue.onClick.AddListener(() => { clickColor("blue"); });
+        bt_select_white.onClick.AddListener(() => { clickColor("white"); });
+        bt_select_black.onClick.AddListener(() => { clickColor("black"); });
+        bt_select_pink.onClick.AddListener(() => { clickColor("pink"); });
+        bt_select_quit.onClick.AddListener(clickQuit);
+        bt_attaque_1.onClick.AddListener(() => { addVotes(0, 1); });
+        bt_attaque_2.onClick.AddListener(() => { addVotes(1, 1); });
+        bt_attaque_1_m.onClick.AddListener(() => { addVotes(0, -1); });
+        bt_attaque_2_m.onClick.AddListener(() => { addVotes(1, -1); });
+        bt_validate.onClick.AddListener(send);
+
+        bt_members_plus.onClick.AddListener(() => { setMembers(1); });
+        bt_members_minus.onClick.AddListener(() => { setMembers(-1); });
+
+        wait_txt.text = str_wait + nb_joueur;
+    }
+    void Start()
+    {
+
+    }
+
+    public void send()
+    {
+        //ici send et compléter en local si pas tout les votes, avec votes aléa
+    }
+
+    public void addVotes(int num,int nb)
+    {
+        if (menu_manager.setVotes(num, nb))
+        {
+            votes_restants_txt.text = str_votes_restants + menu_manager.nb_votes;
+        }
+    }
+
+    public void setMembers(int n)
+    {
+        if (menu_manager.setMembers(n))
+            members_txt.text = str_members + menu_manager.getMembers();
+    }
+    public void clickStart()
+    {
+        //join or create
+        bool state = menu_manager.isConnecting;
+        if (state)
+        {
+            activateMenu("select");
+        }
+    }
+    public void clickColor(string name)
+    {
+        bool state = menu_manager.verifySelectColor(name);
+        if (state)
+        {
+            activateMenu("wait");
+        }
+    }
+    public void clickQuit()
+    {
+        menu_manager.disconnected();
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+              Application.Quit();
+        #endif
+    }
+
+    public void activateMenu(string s)
+    {
+        bool start, select, wait,choice,turn;
+        start = select = wait = choice = turn = false;
+        switch (s)
+        {
+            case "start":
+                start = true;
+                break;
+            case "select":
+                select = true;
+                break;
+            case "wait":
+                wait = true;
+                break;
+            case "choice":
+                choice = true;
+                break;
+            case "turn":
+                turn = true;
+                break;
+            default:
+                break;
+        }
+        StartMenu.SetActive(start);
+        SelectMenu.SetActive(select);
+        WaitingMenu.SetActive(wait);
+        GameChoice.SetActive(choice);
+        GameTurn.SetActive(turn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
